863d8630ea1f97726d0c7efaabc7771fd7da5e10
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2021-09-05 20:49:59 +1000

    Fix linting errors on server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const pool = require("./db");
+
 const PORT = process.env.PORT || 3001;
 
 // Middleware
@@ -11,9 +12,9 @@ app.use(express.json()); // Convert request body to JSON
 
 app.use(express.static(path.resolve(__dirname, '../client/build'))); // Serve the files for the built React app
 
-app.use('/auth', require('./routes/auth'))
+app.use('/auth', require('./routes/auth'));
 
-app.use('/dashboard', require('./routes/dashboard'))
+app.use('/dashboard', require('./routes/dashboard'));
 
 app.get('*', (req, res) => { // Handle all other GET requests by returning the React app
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,18 +1,18 @@
 const express = require('express');
+
 const router = express.Router();
 const pool = require('../db');
-const authorization = require('../middleware/authorization')
+const authorization = require('../middleware/authorization');
 
 router.get('/', authorization, async (req, res) => {
   try {
+    const user = await pool.query('SELECT first_name FROM users WHERE user_id = $1', [req.user]);
 
-    const user = await pool.query("SELECT first_name FROM users WHERE user_id = $1", [req.user]); 
-    
-    res.json({ firstName: user.rows[0].first_name })
+    res.json({ firstName: user.rows[0].first_name });
   } catch (error) {
     console.error(error.message);
-    res.status(500).send("Server error");
+    res.status(500).send('Server error');
   }
-})
+});
 
-module.exports = router
+module.exports = router;
