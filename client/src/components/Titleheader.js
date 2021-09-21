46afa4a90dcaf5b7a2c258f105140bc33ba43472
ConnorKeir <61400871+connorKeir@users.noreply.github.com>
2021-09-21 18:18:11 +1000

    Add user profile pic

--- client/src/components/Titleheader.js
+++ client/src/components/Titleheader.js
@@ -4,26 +4,16 @@ import wretch from 'wretch'
 
 
 function TitleHeader({ title, name }) {
-    const [profileImage, setProfileImage] = useState({})
-
-
-    async function getProfileImage() {
-        const response = await wretch("https://avatars.dicebear.com/api/micah/${name}.svg").get().json()
-        setProfileImage(response)
-    }
-
-    
-    useEffect(() => {
-        getProfileImage()
-    }, [])
 
     return (
         <div className={styles.TitleHeader}> 
             <div className={styles.PageTitle}>
                 <h1>{title}</h1>
             </div>
+            <div className={styles.UserImage}>
+                 <img src={`https://avatars.dicebear.com/api/micah/${name}.svg`} alt="User profile"></img>
+            </div>
             <div className={styles.UserDetails}>
-                <img src={profileImage} alt="User profile"></img>
                 <p>{name}</p>
             </div>
         </div>
