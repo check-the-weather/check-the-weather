a1ffe5008401c55ebcd36e46052285a2046c4653
ConnorKeir <61400871+connorKeir@users.noreply.github.com>
2021-09-25 14:08:14 +1000

    Error in intial commit

--- client/src/Pages/Dashboard/Overview.js
+++ client/src/Pages/Dashboard/Overview.js
@@ -1,13 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import wretch from 'wretch'
-
-<<<<<<< HEAD
 import Sidebar from '../../components/Sidebar'
 import TitleHeader from '../../components/TitleHeader'
-=======
-import Sidebar from 'components/Sidebar'
->>>>>>> 62fafdfc613f042246eb38c0d81474c7929fd720
-
 import styles from './Overview.module.css'
 
 function Overview({ name, logout }) {
