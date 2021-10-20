e6b8b4d3d37ca667c07d829efcfa379ee72332c6
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2021-10-20 20:57:12 +1000

    Fix vertical alignment of profile pic

--- client/src/components/Chatroom.js
+++ client/src/components/Chatroom.js
@@ -39,7 +39,7 @@ function Chatroom({ name }) {
   function renderChat() {
     return chat.map(({ name, message }, index) => (
       <div key={index}>
-        <h3>
+        <h3 className={styles.Row}>
           <img className={styles.UserImage} src={`https://avatars.dicebear.com/api/identicon/${name}.svg`} alt="User profile" />
           {name}: <span className={styles.BlueText}>{message}</span>
         </h3>
