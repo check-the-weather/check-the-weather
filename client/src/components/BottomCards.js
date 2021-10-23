45f5142094b397a871b9626cd1d6e29340a28b1b
Callum Gedling <callum.gedling@my.jcu.edu.au>
2021-10-23 15:48:38 +1000

    Add real values to the sun card

--- client/src/components/BottomCards.js
+++ client/src/components/BottomCards.js
@@ -4,40 +4,38 @@ import BottomCard from './BottomCard'
 import styles from './BottomCards.module.scss' 
 
 function BottomCards({ data }) {
-    const hightide = data?.weatherApiData?.current?.tide ?? ''
-    const lowtide = data?.weatherApiData?.current?.feelslike_c
-    const windspeed = data?.weatherApiData?.current?.wind_kph ?? '' 
-    const winddirection = data?.weatherApiData?.current?.wind_dir ??''
-    const sunrisetime = data?.owmData?.main?.pressure ?? ''
-    const sunsettime = data?.owmData?.main?.pressure ?? ''
-    const uvindex = data?.weatherApiData?.current?.uv ?? '' 
+    const windSpeed = data?.weatherApiData?.current?.wind_kph ?? '' 
+    const windDirection = data?.weatherApiData?.current?.wind_dir ?? ''
+    const sunRiseTime = data?.weatherApiData?.forecast?.forecastday[0]?.astro?.sunrise ?? '' 
+    const sunSetTime = data?.weatherApiData?.forecast?.forecastday[0]?.astro?.sunset ?? '' 
+    const uvIndex = data?.weatherApiData?.current?.uv ?? '' 
 
   
   const FISHING_ROWS = [
     { name: 'Next High Tide', value: '21:00 (5m)'},
     { name: 'Next Low Tide', value: '13:00 (0.85m)'},
-    { name: 'Wind Speed', value: windspeed + " km/h"},
-    { name: 'Wind Direction', value: winddirection},
+    { name: 'Wind Speed', value: windSpeed + " km/h"},
+    { name: 'Wind Direction', value: windDirection},
   ];
 
   const SUN_ROWS = [
-    { name: 'Sunrise', value: '05:00'},
-    { name: 'Best Sunrise Photo Opportunity', value: '05:15'},
-    { name: 'Sunset', value: '17:15'},
-    { name: 'Best Sunset Photo Opportunity', value: '17:55'},
+    { name: 'Sunrise', value: sunRiseTime},
+    { name: 'Best Sunrise Photo Opportunity', value:sunRiseTime },
+    { name: 'Sunset', value: sunSetTime },
+    { name: 'Best Sunset Photo Opportunity', value: sunSetTime},
   ];
 
   
-  function getWaterStatus(windspeed) {
-    if (windspeed >= 20) {
+  function getWaterStatus(windSpeed) {
+    if (windSpeed >= 20) {
       return { code: 'bad', message: 'Poor Conditions'}
     } 
     
-    else if (windspeed > 15 && windspeed <= 20) {
+    else if (windSpeed > 15 && windSpeed <= 20) {
       return { code: 'medium', message: 'Fair Conditions'}
     }
 
-    else if (windspeed <= 15){
+    else if (windSpeed <= 15){
       return { code: 'good', message: 'Good Conditions'}
     }
     else {
@@ -45,24 +43,24 @@ function BottomCards({ data }) {
     }
   }
 
-  function updateSunStatus(uvindex){
-    if (uvindex > 5) {
-      return { code: 'bad', message: `UV Index of ${uvindex}`}
+  function updateSunStatus(uvIndex){
+    if (uvIndex > 5) {
+      return { code: 'bad', message: `UV Index of ${uvIndex}`}
     }
-    else if (uvindex >= 3 && uvindex < 6) {
-      return { code: 'medium', message: `UV Index of ${uvindex}`} 
+    else if (uvIndex >= 3 && uvIndex < 6) {
+      return { code: 'medium', message: `UV Index of ${uvIndex}`} 
     }
 
-    else if (uvindex <= 2){
-      return { code: 'good', message: `UV Index of ${uvindex}`}
+    else if (uvIndex <= 2){
+      return { code: 'good', message: `UV Index of ${uvIndex}`}
     }
     else {
       return {code: 'unknown', message: ''}
     }
   }
 
-  const waterStatus = getWaterStatus(windspeed)
-  const sunStatus = updateSunStatus(uvindex)
+  const waterStatus = getWaterStatus(windSpeed)
+  const sunStatus = updateSunStatus(uvIndex)
 
   return (
     <Group className={styles.BottomCards}>
